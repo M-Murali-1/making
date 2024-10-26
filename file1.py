9bad6e08d6f790575c204bf38f4419e6838cf3f3
Murali <murali15062003@gmail.com>
2024-10-26 12:14:51 +0530

    Second Commit Done

--- file1.py
+++ file1.py
@@ -1,2 +1,4 @@
 printing the name is not the thing that er are going to do..!
 making the thing that are going to be in the good manner..!
+mkaing the things will be the better for any one..!
+    me and my things are the local one  //Commit done1
