b32c13729bc9d68891c31e67dfa9ed09c93f196f
Murali <106273849+M-Murali-1@users.noreply.github.com>
2024-10-26 12:15:37 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -2,3 +2,4 @@ printing the name is not the thing that er are going to do..!
 making the thing that are going to be in the good manner..!
 mkaing the things will be the better for any one..!
     me and my things are the local one  //Commit done1
+Adding the things in the Remote repository //Comit done in the remote..
