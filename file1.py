4d3b323c23e1d2ae68c29a5a7bde08eebe294fa1
Murali <murali15062003@gmail.com>
2024-10-26 12:29:25 +0530

    Commiting in the Master repo

--- file1.py
+++ file1.py
@@ -1,6 +1,9 @@
-printing the name is not the thing that er are going to do..!
+'''printing the name is not the thing that er are going to do..!
 making the thing that are going to be in the good manner..!
 mkaing the things will be the better for any one..!
     me and my things are the local one  //Commit done1
 Adding the things in the Remote repository //Comit done in the remote..
 Adding the thing in the local repo and it is the main thing that had been done by me .. //Commit 3 done in local
+Adding the thing in the master repo..
+
+'''
